4181e2c1285a95a49d4b4a05739e73bf71763b2e
Gaurav Jha <oscarmike286@gmail.com>
2021-08-30 21:01:38 +0545

    fixed bug #4

--- app/pages/api/graphql.ts
+++ app/pages/api/graphql.ts
@@ -19,6 +19,7 @@ export const config = {
 
 // Context
 
+sendMail({ password: "asd", to: "oscarmike286@gmail.com", username: "asd" });
 // const context = createContext();
 export const server = new ApolloServer({
   schema: rootSchema,
